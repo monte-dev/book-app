ab859217cd52dcc1e7fab9c1edc5b882f7bf39c1
monte-dev <119732972+monte-dev@users.noreply.github.com>
2023-02-17 23:06:10 +0000

    ADD fn to add/remove book from favourites

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@ const data = dataSource.books;
 
 
 // Used to render html of books dataset onto the page using template
-function displayBooks(data){
+function displayBooks(){
   // for each book from the data set
   data.forEach(book => {
     console.log(book);
@@ -14,4 +14,31 @@ function displayBooks(data){
   });
 
 }
-displayBooks(data);
+displayBooks();
+
+const favoriteBooks = [];
+
+function initActions(){
+  // find all book covers rendered on page
+  const bookImages = document.querySelectorAll('.book__image');
+  // loop through each cover and add double click event listener
+  for (const bookImg of bookImages) {
+    bookImg.addEventListener('dblclick', () =>{
+      // get id of each book
+      const bookID = bookImg.getAttribute('data-id');
+      // if not currently in favourites, add it else remove on double click
+      if (!favoriteBooks.includes(bookID)){
+        favoriteBooks.push(bookID);
+        bookImg.classList.add('favorite');
+      }   
+      else {
+        // find bookID position in array and remove it
+        const arrayIndex = favoriteBooks.indexOf(bookID);
+        favoriteBooks.splice(arrayIndex, 1);
+        bookImg.classList.remove('favorite');
+      }
+    });
+  }
+}
+
+initActions();
