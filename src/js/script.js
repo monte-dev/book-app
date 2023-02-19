a6b2ba2d5e7d5da370e8a431079a09f8257b5a5e
monte-dev <119732972+monte-dev@users.noreply.github.com>
2023-02-19 15:04:20 +0000

    CHANGE code to make use of classes and OOP approach

--- src/js/script.js
+++ src/js/script.js
@@ -1,104 +1,118 @@
-/* eslint-disable no-empty */
-/* eslint-disable no-unused-vars */
-const booksListEl = document.querySelector('.books-list');
-const bookTemplateEl = document.querySelector('#template-book');
-const filtersEl = document.querySelector('.filters');
-const data = dataSource.books;
+class BooksList {
+  constructor(data) {
+    this.data = data;
 
-// Used to render html of books dataset onto the page using template
-function displayBooks() {
-	// for each book from the data set
-	data.forEach((book) => {
-		// convert to a value used in %
-		const ratingWidth = book.rating * 10;
-		const ratingBgc = applyRatingColor(book.rating);
-		// use spread syntax to assign new values ratingWidth and ratingBgc to object book
-		const generateHTML = Handlebars.compile(bookTemplateEl.innerHTML)({
-			...book,
-			ratingWidth,
-			ratingBgc,
-		});
-		const container = utils.createDOMFromHTML(generateHTML);
-		booksListEl.appendChild(container);
-	});
-}
-displayBooks();
+    this.initData();
+    this.getElements();
+    this.displayBooks();
+    this.initActions();
+    this.applyFilters();
+    this.applyRatingColor();
+  }
+  // find data
+  initData() {
+    this.data = dataSource.books;
+    console.log('Data:', this.data);
+    
+  }
+  // find elements used throughout class.
+  getElements() {
+    this.booksListEl = document.querySelector('.books-list');
+    this.bookTemplateEl = document.querySelector('#template-book');
+    this.filtersEl = document.querySelector('.filters');
+  }
+  // render books from dataset onto html
+  displayBooks() {
+    this.data.forEach((book) => {
+    // convert to a value used in %
+      const ratingWidth = book.rating * 10;
+      const ratingBgc = this.applyRatingColor(book.rating);
+      // use spread syntax to assign new values ratingWidth and ratingBgc to object book
+      const generateHTML = Handlebars.compile(this.bookTemplateEl.innerHTML)({...book,	ratingWidth, ratingBgc,});
+      const container = utils.createDOMFromHTML(generateHTML);
+      this.booksListEl.appendChild(container);
+    });
+  }
+  // Add event listeners to filters elements and book covers to add them to favourites
+  initActions() {
+    const favoriteBooks = [];
+    // FAVOURITES HANDLING
+    this.booksListEl.addEventListener('dblclick', (event) => {
+    // assign link(parent) of book image to var
+      const book = event.target.offsetParent;
+      if (book.classList.contains('book__image')) {
+      // get id of each book
+        const bookID = book.getAttribute('data-id');
+        // if not currently in favourites, add it else remove on double click
+        if (!favoriteBooks.includes(bookID)) {
+          favoriteBooks.push(bookID);
+          book.classList.add('favorite');
+        } else {
+        // find bookID position in array and remove it
+          const arrayIndex = favoriteBooks.indexOf(bookID);
+          favoriteBooks.splice(arrayIndex, 1);
+          book.classList.remove('favorite');
+        }
+      }
+    });
+    this.filters = [];
 
-const favoriteBooks = [];
-function initActions() {
-	// FAVOURITES HANDLING
-	booksListEl.addEventListener('dblclick', (event) => {
-		// assign link(parent) of book image to var
-		const book = event.target.offsetParent;
-		if (book.classList.contains('book__image')) {
-			// get id of each book
-			const bookID = book.getAttribute('data-id');
-			// if not currently in favourites, add it else remove on double click
-			if (!favoriteBooks.includes(bookID)) {
-				favoriteBooks.push(bookID);
-				book.classList.add('favorite');
-			} else {
-				// find bookID position in array and remove it
-				const arrayIndex = favoriteBooks.indexOf(bookID);
-				favoriteBooks.splice(arrayIndex, 1);
-				book.classList.remove('favorite');
-			}
-		}
-	});
-
-	// FILTERS FORM
-	filtersEl.addEventListener('click', (e) => {
-		// check if clicked element in filters form is an input checkbox
-		if (
-			e.target.tagName === 'INPUT' &&
+    // FILTERS FORM
+    this.filtersEl.addEventListener('click', (e) => {
+    // check if clicked element in filters form is an input checkbox
+      if (
+        e.target.tagName === 'INPUT' &&
 			e.target.type === 'checkbox' &&
 			e.target.name === 'filter'
-		) {
-			// add/remove checked checkbox to array of filters
-			if (e.target.checked) {
-				filters.push(e.target.value);
-			} else {
-				const indexUnchecked = filters.indexOf(e.target.value);
-				filters.splice(indexUnchecked, 1);
-			}
-			applyFilters();
-		}
-	});
-}
-initActions();
+      ) {
+      // add/remove checked checkbox to array of filters
+        if (e.target.checked) {
+          this.filters.push(e.target.value);
+        } else {
+          const indexUnchecked = this.filters.indexOf(e.target.value);
+          this.filters.splice(indexUnchecked, 1);
+        }
 
-const filters = [];
-
-function applyFilters() {
-	for (let book of dataSource.books) {
-		let isHidden = false;
-		for (const filter of filters) {
-			if (!book.details[filter]) {
-				isHidden = true;
-				break;
-			}
-		}
-		// Find book__image element for current book
-		const bookImg = document.querySelector(
-			`.book__image[data-id="${book.id}"]`
-		);
-		// apply/remove class hidden depending which filter is selected
-		if (isHidden) {
-			bookImg.classList.add('hidden');
-		} else {
-			bookImg.classList.remove('hidden');
-		}
-	}
+        this.applyFilters(this.filters);
+      }
+    });
+    return this.filters;
+  }
+  // loop through books in data set and apply filters , then grab book by ID and add/remove css to filtered books
+  applyFilters() {
+    for (let book of this.data) {
+      let isHidden = false;
+      for (const filter of this.filters) {
+        if (!book.details[filter]) {
+          isHidden = true;
+          break;
+        }
+      }
+      // Find book__image element for current book
+      const bookImg = document.querySelector(
+        `.book__image[data-id="${book.id}"]`
+      );
+      // apply/remove class hidden depending which filter is selected
+      if (isHidden) {
+        bookImg.classList.add('hidden');
+      } else {
+        bookImg.classList.remove('hidden');
+      }
+    }
+    
+  }
+  // use rating called in displayBooks method to apply styles
+  applyRatingColor(rating) {
+    if (rating <= 6) {
+      return 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)';
+    } else if (rating > 6 && rating <= 8) {
+      return 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
+    } else if (rating > 8 && rating <= 9) {
+      return 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
+    } else if (rating > 9) {
+      return 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
+    }
+  }
 }
-
-function applyRatingColor(rating) {
-	if (rating <= 6) {
-		return 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)';
-	} else if (rating > 6 && rating <= 8) {
-		return 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
-	} else if (rating > 8 && rating <= 9) {
-		return 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
-	} else if (rating > 9) {
-		return 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
-	}
-}
+// eslint-disable-next-line no-unused-vars
+const app = new BooksList();
